dcda7ffb8ff50091e754c637ef9b74e13a689f72
San_AI <santiagoa.bernalg@konradlorenz.edu.co>
2024-07-05 23:16:06 -0500

    Añadir implementación de la clase Inventory

---
+++ src/com/inventory/controller/AplMain.java
@@ -0,0 +1,7 @@
+package com.inventory.controller;
+
+public class AplMain {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/com/inventory/model/Inventory.java
@@ -0,0 +1,20 @@
+package com.inventory.model;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class Inventory {
+
+    private Map<Integer, Product> products;
+
+    public Inventory() {
+        this.products = new HashMap<>();
+    }
+
+    // Metods
+
+    public void addProduct(Product product){
+
+    }
+
+}
