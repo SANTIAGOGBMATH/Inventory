934b341e97e08e18a4e06348788a7b4642d2a9f6
San_AI <santiagoa.bernalg@konradlorenz.edu.co>
2024-07-05 23:52:06 -0500

    Class Inventory created

--- src/com/inventory/model/Inventory.java
+++ src/com/inventory/model/Inventory.java
@@ -14,7 +14,31 @@ public class Inventory {
     // Metods
 
     public void addProduct(Product product){
+    if (products.containsKey(product.getProductId())){
+        System.out.println("This product is already in the inventory" + product.getProductId());
+        return;
+    }
+    products.put(product.getProductId(), product);
+    System.out.printf("Product %s added to the inventory\n", product.getProductId());
+    }
 
+    public void removeProduct(int productId){
+        if (!products.containsKey(productId)){
+             System.out.println("This product is not in the inventory" + productId);
+        }
+        products.remove(productId);
+        System.out.printf("Product %s removed from the inventory\n", productId);
+    }
+    public void updateProduct(Product product){
+        if (!products.containsKey(product.getProductId())){
+            System.out.println("This product is not in the inventory" + product.getProductId());
+        }
+        products.put(product.getProductId(), product);
+        System.out.printf("Product %s updated to the inventory\n", product.getProductId());
+    }
+
+    public Product findProduct(int productId){
+        return products.get(productId);
     }
 
 }
